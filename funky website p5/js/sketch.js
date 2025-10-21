d00203dadf120bf79419617070ada779f655e7ff
prawnavi <pranavi.kristipati06@gmail.com>
2025-10-21 13:25:44 -0700

    updtae

---
+++ funky website p5/js/sketch.js
@@ -0,0 +1,39 @@
+
+let canvas; 
+let xPos = 0;
+let yPos = 0;
+let easing= .5;
+
+function setup(){
+    canvas = createCanvas (windowWidth, document.body.scrollHeight);
+    canvas.style ("z-index",-2)
+    canvas.position(0,0);
+    background (225);
+}
+
+function windowResized(){
+    resizeCanvas (windowWidth, windowHeight);
+
+}
+
+function draw(){
+    clear();
+
+  xPos = xPos + ((mouseX - xPos) * easing);
+  yPos = yPos + ((mouseY - yPos) * easing);
+
+    drawThing(xPos, yPos);
+}
+
+function mouseMoved(){
+}
+
+function drawThing(_x, _y){
+fill (255);
+ellipse(_x, _y, 30, 30);
+ellipse(_x -20, _y + 5, 30, 30);
+
+fill(0);
+ellipse(_x + 10, _y, 5, 5);
+ellipse(_x - 10, _y + 5, 5, 5);
+}
